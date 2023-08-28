a76e15ac4a8f908d326d188cf1b6f643aaa70c79
Gulshan <kumarjhagulshan@gmail.com>
2023-08-28 10:59:35 +0530

    initial env files commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    script: "./index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    client: {
+      overlay: true,
+    },
+    hot: true,
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: ["*.html"],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new Dotenv(),
+  ],
+};
